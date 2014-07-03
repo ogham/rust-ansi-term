33b632e3d926daa55a697d82430412cc588adafa
Ben S <ogham@bsago.me>
2014-07-03 15:04:34 +0100

    Add rustdoc comments

--- src/ansi_term.rs
+++ src/ansi_term.rs
@@ -3,9 +3,86 @@
 #![crate_type = "dylib"]
 #![desc = "A rust library for ANSI terminal colours and styles (bold, underline)"]
 
+//! This is a library for controlling colours and formatting, such as
+//! red bold text or blue underlined text, on ANSI terminals.
+//!
+//! Simple Colours
+//! --------------
+//!
+//! You can format strings by calling the `paint` method on a Colour
+//! or a Style object, passing in the string you want to format. For
+//! example, to get some red text, call the `paint` method on `Red`:
+//!
+//! ```rust
+//! Red.paint("Red!")
+//! ```
+//!
+//! You can also call the `paint` method on a Style. To turn a Colour
+//! into a Style, use the `normal` method, though this does the exact
+//! some thing as above:
+//!
+//! ```rust
+//! Red.normal().paint("Still red!")
+//! ```
+//!
+//! Bold, Underline, and Background
+//! -------------------------------
+//!
+//! To do anything more complex than just foreground colours, you need
+//! to use Style objects. Calling the `bold` or `underline` method on
+//! a Colour returns a Style that has the appropriate property set on
+//! it:
+//!
+//! ```rust
+//! Blue.bold().paint("Blue bold!")
+//! Yellow.underline().paint("Yellow underline!")
+//! ```
+//!
+//! Again, you can call these methods on Styles as well as just Colours:
+//!
+//! ```rust
+//! Blue.normal().bold().paint("Still blue bold!")
+//! ```
+//!
+//! Finally, you can set the background colour of a Style by using the
+//! `on` method:
+//!
+//! ```rust
+//! Blue.on(Yellow).paint("Blue on yellow!")
+//! ```
+//!
+//! Extended Colours
+//! ----------------
+//!
+//! You can access the extended range of 256 colours by using the
+//! Fixed constructor, which takes an argument of the colour number to
+//! use. This can be used wherever you would use a Colour:
+//!
+//! ```rust
+//! Fixed(134).paint("A sort of light purple.")
+//! ```
+//!
+//! This even works for background colours:
+//!
+//! ```rust
+//! Fixed(221).on(Fixed(124)).paint("Mustard in the ketchup.")
+//! ```
+//!
+//! No Formatting
+//! -------------
+//!
+//! Finally, for the sake of completeness, the Plain style provides
+//! neither colours nor formatting.
+//!
+//! ```rust
+//! Plain.paint("No colours here.")
+//! ```
+
 #![feature(phase)] extern crate regex;
 #[phase(plugin)] extern crate regex_macros;
 
+/// A colour is one specific type of ANSI escape code, and can refer
+/// to either the foreground or background colour.
 pub enum Colour {
     Black, Red, Green, Yellow, Blue, Purple, Cyan, White, Fixed(u8),
 }
@@ -40,24 +117,26 @@ impl Colour {
             White => "47".to_string(),
             Fixed(num) => format!("48;5;{}", num),
         }
-    }        
+    }
 }
 
-// There are only three different styles: plain (no formatting), only
-// a foreground colour, and a catch-all for anything more complicated
-// than that. It's technically possible to write other cases such as
-// "bold foreground", but probably isn't worth writing all the code.
-
+/// A style is a collection of properties that can format a string
+/// using ANSI escape codes.
 pub enum Style {
+    /// The Plain style provides no formatting.
     Plain,
+    /// The Foreground style provides just a foreground colour.
     Foreground(Colour),
+    /// The Style style is a catch-all for anything more complicated
+    /// than that. It's technically possible for there to be other
+    /// cases, such as "bold foreground", but probably isn't worth it.
     Style(StyleStruct),
 }
 
 // Having a struct inside an enum is currently unfinished in Rust, but
 // should be put in there when that feature is complete.
 
-pub struct StyleStruct {
+struct StyleStruct {
     foreground: Colour,
     background: Option<Colour>,
     bold: bool,
@@ -65,6 +144,8 @@ pub struct StyleStruct {
 }
 
 impl Style {
+    /// Paints the given text with this style, returning a String of
+    /// text with the relevant ANSI escape codes added.
     pub fn paint(&self, input: &str) -> String {
         match *self {
             Plain => input.to_string(),
@@ -83,9 +164,8 @@ impl Style {
             }
         }
     }
-}
 
-impl Style {
+    /// Return a Style with the bold property set.
     pub fn bold(&self) -> Style {
         match *self {
             Plain => Style(StyleStruct         { foreground: White,         background: None,          bold: true, underline: false }),
@@ -94,6 +174,7 @@ impl Style {
         }
     }
 
+    /// Return a Style with the underline property set.
     pub fn underline(&self) -> Style {
         match *self {
             Plain => Style(StyleStruct         { foreground: White,         background: None,          bold: false,   underline: true }),
@@ -102,6 +183,7 @@ impl Style {
         }
     }
 
+    /// Return a Style with the background colour set.
     pub fn on(&self, background: Colour) -> Style {
         match *self {
             Plain => Style(StyleStruct         { foreground: White,         background: Some(background), bold: false,   underline: false }),
@@ -112,34 +194,41 @@ impl Style {
 }
 
 impl Colour {
-    // This is a short-cut so you don't have to use Blue.normal() just
-    // to turn Blue into a Style. Annoyingly, this means that Blue and
-    // Blue.normal() aren't of the same type, but this hasn't been an
-    // issue so far.
-    
+    /// Paints the given text with this colour. This is a short-cut so
+    /// you don't have to use Blue.normal() just to get blue text.
     pub fn paint(&self, input: &str) -> String {
         let re = format!("\x1B[{}m{}\x1B[0m", self.foreground_code(), input);
         return re.to_string();
     }
 
+    /// Returns a Style with the underline property set.
     pub fn underline(&self) -> Style {
         Style(StyleStruct { foreground: *self, background: None, bold: false, underline: true })
     }
 
+    /// Return a Style with the bold property set.
     pub fn bold(&self) -> Style {
         Style(StyleStruct { foreground: *self, background: None, bold: true, underline: false })
     }
 
+    /// Return a Style with the foreground colour set to this colour.
     pub fn normal(&self) -> Style {
         Style(StyleStruct { foreground: *self, background: None, bold: false, underline: false })
     }
 
+    /// Return a Style with the background colour set.
     pub fn on(&self, background: Colour) -> Style {
         Style(StyleStruct { foreground: *self, background: Some(background), bold: false, underline: false })
     }
 }
 
+/// Return a String with all ANSI escape codes removed. For example:
+///
+/// ```rust
+/// strip_formatting(Blue.paint("hello!")) == "hello!"
+/// ```
 pub fn strip_formatting(input: String) -> String {
+    // What's blue and smells like red paint? Blue paint.
     let re = regex!("\x1B\\[.+?m");
     re.replace_all(input.as_slice(), "").to_string()
 }
@@ -147,13 +236,13 @@ pub fn strip_formatting(input: String) -> String {
 #[cfg(test)]
 mod tests {
     use super::{Black, Red, Green, Yellow, Blue, Purple, Cyan, White, Fixed, Plain, strip_formatting};
-    
+
     #[test]
     fn test_plain() {
         let hi = Plain.paint("text/plain");
         assert!(hi == "text/plain".to_string());
     }
-    
+
     #[test]
     fn test_red() {
         let hi = Red.paint("hi");
@@ -249,13 +338,13 @@ mod tests {
         let hi = Fixed(100).on(Fixed(200)).paint("hi");
         assert!(hi == "\x1B[48;5;200;38;5;100mhi\x1B[0m".to_string());
     }
-    
+
     #[test]
     fn test_strip_formatting() {
         let hi = strip_formatting(Blue.paint("hi"));
         assert!(hi == "hi".to_string());
     }
-    
+
     #[test]
     fn test_strip_formatting_2() {
         let hi = strip_formatting(Blue.on(Fixed(230)).bold().paint("hi"));
