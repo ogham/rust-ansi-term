cd0886a24aa3ef289c35cd1b946ec8d651bdd45d
Sergey Galich <sergey.galich@gmail.com>
2018-07-21 23:24:30 -0400

    fix https://github.com/ogham/rust-ansi-term/issues/42

--- src/windows.rs
+++ src/windows.rs
@@ -11,6 +11,7 @@ pub fn enable_ansi_support() -> Result<(), u32> {
     use winapi::um::processenv::GetStdHandle;
     use winapi::um::errhandlingapi::GetLastError;
     use winapi::um::consoleapi::{GetConsoleMode, SetConsoleMode};
+    use winapi::um::handleapi::INVALID_HANDLE_VALUE;
 
     const STD_OUT_HANDLE: u32 = -11i32 as u32;
     const ENABLE_VIRTUAL_TERMINAL_PROCESSING: u32 = 0x0004;
@@ -18,21 +19,25 @@ pub fn enable_ansi_support() -> Result<(), u32> {
     unsafe {
         // https://docs.microsoft.com/en-us/windows/console/getstdhandle
         let std_out_handle = GetStdHandle(STD_OUT_HANDLE);
-        let error_code = GetLastError();
-        if error_code != 0 { return Err(error_code); }
+        if std_out_handle == INVALID_HANDLE_VALUE
+        {
+            return Err(GetLastError()); 
+        }
         
         // https://docs.microsoft.com/en-us/windows/console/getconsolemode
         let mut console_mode: u32 = 0;
-        GetConsoleMode(std_out_handle, &mut console_mode);
-        let error_code = GetLastError();
-        if error_code != 0 { return Err(error_code); }
+        if 0 == GetConsoleMode(std_out_handle, &mut console_mode)
+        {
+            return Err(GetLastError());
+        }
 
         // VT processing not already enabled?
         if console_mode & ENABLE_VIRTUAL_TERMINAL_PROCESSING == 0 {
             // https://docs.microsoft.com/en-us/windows/console/setconsolemode
-            SetConsoleMode(std_out_handle, console_mode | ENABLE_VIRTUAL_TERMINAL_PROCESSING);
-            let error_code = GetLastError();
-            if error_code != 0 { return Err(error_code); }
+            if 0 == SetConsoleMode(std_out_handle, console_mode | ENABLE_VIRTUAL_TERMINAL_PROCESSING)
+            {
+                return Err(GetLastError()); 
+            }
         }
     }
 
